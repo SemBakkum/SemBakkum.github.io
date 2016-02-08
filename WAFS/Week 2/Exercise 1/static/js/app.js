190b3cb7dcf8b6006aebb12e61a70be43a257294
Sembah <macbook@059.125.028.145.hva.nl>
2016-02-08 16:56:05 +0100

    Changed api link
    For better results from the API

--- WAFS/Week 2/Exercise 1/static/js/app.js
+++ WAFS/Week 2/Exercise 1/static/js/app.js
@@ -12,7 +12,7 @@
 
 			var wheater = document.getElementById('demo');
 
-			microAjax("http://api.openweathermap.org/data/2.5/weather?q=Amsterdam&units=metric&appid=44db6a862fba0b067b1930da0d769e98", function(data){
+			microAjax("http://api.openweathermap.org/data/2.5/weather?id=2759794&units=metric&appid=44db6a862fba0b067b1930da0d769e98", function(data){
 				data = JSON.parse(data);
 				var wheaterData = {
 					city: data.name,
