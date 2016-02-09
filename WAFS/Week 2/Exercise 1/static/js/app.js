642568a4bdd55675d76b632377f1549bd1e17865
Sembah <macbook@059.125.028.145.hva.nl>
2016-02-09 10:27:57 +0100

    Date changes

--- WAFS/Week 2/Exercise 1/static/js/app.js
+++ WAFS/Week 2/Exercise 1/static/js/app.js
@@ -20,10 +20,12 @@
 					weatherType: data.weather[0].description,
 					weatherIcon: data.weather[0].icon,
 					country: data.sys.country,
-					sunDown: data.sys.sunset
+					sunUp: data.sys.sunrise,
+					sunDown: data.sys.sunset,
+					gotData: data.dt,
+					windSpeed: data.wind.speed
 				}
-				console.log(weatherData.weather);
-				console.log(weatherData.sunUp);
+				console.log(weatherData.windSpeed)
 
 				var iconImg = document.getElementById('weatherIcon');
 
@@ -34,6 +36,16 @@
 				var newSunDown = new Date(sunDownValue*1000);
 				document.getElementById('sunSetValue').innerHTML = newSunDown;
 				console.log(newSunDown);
+
+				var sunUpValue = parseInt(weatherData.sunUp);
+				var newSunUp = new Date(sunUpValue*1000);
+				document.getElementById('sunUpValue').innerHTML = newSunUp;
+				console.log(newSunUp);
+
+				var gotDataValue = parseInt(weatherData.gotData);
+				var newGotData = new Date(gotDataValue*1000);
+				document.getElementById('gotDataValue').innerHTML = newGotData;
+				console.log(newGotData);				
 
 				Transparency.render(weather, weatherData);
 			})
