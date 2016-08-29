158878f601d316c7edd39c9428a369a21fef4567
Sem B <sem.bakkum@gmail.com>
2016-08-29 16:17:34 +0200

    Update request.js

--- WAFS/Week 3/static/js/modules/request.js
+++ WAFS/Week 3/static/js/modules/request.js
@@ -3,11 +3,11 @@
 var retrieve = (function(){
 
 	var currentData = function(city, cb) {
-		microAjax('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=nl&appid=44db6a862fba0b067b1930da0d769e98', cb)
+		microAjax('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&lang=nl&appid=992bfff1b942695c9266d29b8f3a0ab7', cb)
 	}
 
 	var forecastData = function(city, cb) {
-		microAjax('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&units=metric&cnt=7&lang=nl&appid=44db6a862fba0b067b1930da0d769e98', cb)
+		microAjax('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&units=metric&cnt=7&lang=nl&appid=992bfff1b942695c9266d29b8f3a0ab7', cb)
 	}
 
 	return {
